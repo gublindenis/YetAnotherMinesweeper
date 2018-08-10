6284d5b1efab732af6ce9b48dd933bd63092c25e
gublindenis <denis@WIN8_1>
2018-08-10 23:32:24 +0300

    добавил класс-ячейку

---
+++ YetAnotherMinesweeper/ViewModel/Cell.cs
@@ -0,0 +1,41 @@
+﻿using Minesweeper.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetAnotherMinesweeper.ViewModel
+{
+    public class Cell : ObservableObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int BombQuantityAround { get; set; }
+        public bool IsBomb { get; set; }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        
+
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+
+    }
+}
