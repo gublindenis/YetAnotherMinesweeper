1c57c5bcfec39236bf4b7b65c0d6d98923ccbcbc
gublindenis <denis@WIN8_1>
2018-08-11 01:15:22 +0300

    проект под юнит тесты

---
+++ YetAnotherMinesweeper_UnitTests/ViewModel/Minefield_Tests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YetAnotherMinesweeper.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetAnotherMinesweeper.ViewModel.UnitTests
+{
+    [TestClass()]
+    public class Minefield_Tests
+    {
+        [TestMethod()]
+        public void GetIndexesAround_Test_01()
+        {
+            PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+            List<int> expected = (new List<int> { 0, 1, 4, 6, 7 });
+            expected.Sort();
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 0, 1);
+            actual.Sort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+ 
+        [TestMethod()]
+        public void GetIndexesAround_Test_11()
+        {
+            PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+            List<int> expected = (new List<int> { 0,1,2,3,5,6,7,8 });
+            expected.Sort();
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 1, 1);
+            actual.Sort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
