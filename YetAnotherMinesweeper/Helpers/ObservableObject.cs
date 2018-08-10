a917c17a029838abdf0140bf87ea6ad2860d779e
gublindenis <denis@WIN8_1>
2018-08-10 23:22:19 +0300

    базовый класс, чтобы при изменении свойств возникало событие

---
+++ YetAnotherMinesweeper/Helpers/ObservableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Minesweeper.Helpers
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        protected void RaisePropertyChanged(params string[] propertyNames)
+        {
+            foreach (var name in propertyNames)
+            {
+                RaisePropertyChanged(name);
+            }
+        }
+    }
+}
