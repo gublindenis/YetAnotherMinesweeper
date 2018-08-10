7077ae30de6735f20a92109376d212f33b775b26
gublindenis <denis@WIN8_1>
2018-08-11 01:31:57 +0300

    тесты на метод для обхода соседних ячеек

--- YetAnotherMinesweeper_UnitTests/ViewModel/Minefield_Tests.cs
+++ YetAnotherMinesweeper_UnitTests/ViewModel/Minefield_Tests.cs
@@ -11,26 +11,98 @@ namespace YetAnotherMinesweeper.ViewModel.UnitTests
     [TestClass()]
     public class Minefield_Tests
     {
+        /*
+              [TestMethod()]
+              public void GetIndexesAroundTest00()
+              {
+                  PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+                  List<int> expected = (new List<int> { 1, 3, 4 });
+                  expected.Sort();
+                  List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 0, 0);
+                  actual.Sort();
+                  CollectionAssert.AreEqual(expected, actual);
+              }
+               [TestMethod()]
+              public void GetIndexesAroundTest01()
+              {
+                  PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+                  List<int> expected = (new List<int> { 0, 1, 4, 6, 7 });
+                  expected.Sort();
+                  List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 0, 1);
+                  actual.Sort();
+                  CollectionAssert.AreEqual(expected, actual);
+              }
+       */
         [TestMethod()]
-        public void GetIndexesAround_Test_01()
+        public void GetIndexesAroundTest02()
         {
             PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
-            List<int> expected = (new List<int> { 0, 1, 4, 6, 7 });
+            List<int> expected = (new List<int> { 3, 4, 7 });
             expected.Sort();
-            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 0, 1);
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 0, 2);
             actual.Sort();
             CollectionAssert.AreEqual(expected, actual);
         }
- 
         [TestMethod()]
-        public void GetIndexesAround_Test_11()
+        public void GetIndexesAroundTest10()
         {
             PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
-            List<int> expected = (new List<int> { 0,1,2,3,5,6,7,8 });
+            List<int> expected = (new List<int> { 0, 2, 3, 4, 5 });
+            expected.Sort();
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 1, 0);
+            actual.Sort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetIndexesAroundTest11()
+        {
+            PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+            List<int> expected = (new List<int> { 0, 1, 2, 3, 5, 6, 7, 8 });
             expected.Sort();
             List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 1, 1);
             actual.Sort();
             CollectionAssert.AreEqual(expected, actual);
         }
+        [TestMethod()]
+        public void GetIndexesAroundTest12()
+        {
+            PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+            List<int> expected = (new List<int> { 3, 4, 5, 6, 8 });
+            expected.Sort();
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 1, 2);
+            actual.Sort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetIndexesAroundTest20()
+        {
+            PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+            List<int> expected = (new List<int> { 1, 4, 5 });
+            expected.Sort();
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 2, 0);
+            actual.Sort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetIndexesAroundTest21()
+        {
+            PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+            List<int> expected = (new List<int> { 1, 2, 4, 7, 8 });
+            expected.Sort();
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 2, 1);
+            actual.Sort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetIndexesAroundTest22()
+        {
+            PrivateObject privateHelperObject = new PrivateObject(new Minefield(3, 3, 0));
+            List<int> expected = (new List<int> { 4, 5, 7 });
+            expected.Sort();
+            List<int> actual = (List<int>)privateHelperObject.Invoke("GetIndexesAround", 2, 2);
+            actual.Sort();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
     }
 }
