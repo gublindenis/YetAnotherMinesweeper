74599884c378b0173d5b6aafe19e64d5b2c04959
gublindenis <denis@WIN8_1>
2018-08-11 00:25:39 +0300

    enum со статусами ячейки

---
+++ YetAnotherMinesweeper/ViewModel/CellStates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetAnotherMinesweeper.ViewModel
+{
+    public enum CellStates
+    {
+        CLOSE,
+        MARKED,
+        OPEN_CLEAR,
+        OPEN_BOMB
+    }
+}
